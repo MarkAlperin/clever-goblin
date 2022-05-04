22f58d4bde160ad330fde42297d9d3aa88e2f813
MarkAlperin <mark.t.alperin@gmail.com>
2022-05-03 21:55:14 -0500

    monster-search roughly done, moving on to database

--- App.js
+++ App.js
@@ -1,32 +1,41 @@
 // import { StatusBar } from "expo-status-bar";
-import React,  from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
-  Image,
+  ImageBackground,
   ScrollView,
   TextInput,
   StyleSheet,
 } from "react-native";
 
 import SearchBar from "./client/components/SearchBar.js";
-import Banner from "./client/components/Banner.js";
 import Monster from "./client/components/Monster.js";
+import Banner from "./client/components/Banner.js";
 
 const App = () => {
+  const [monster, setMonster] = useState(null);
+  const [bannerMessage, setBannerMessage] = useState(
+    "I'm not trying kill you... but the Goblins are!"
+  );
+
+  const setMessage = (message) => {
+    setBannerMessage(message);
+  };
+
   return (
     <ScrollView>
-      <Banner />
+      <Banner message={bannerMessage} />
 
       <View style={styles.container}>
-        <SearchBar />
-        <Image
+        <SearchBar setMonster={setMonster} setMessage={setMessage} />
+        <ImageBackground
           source={{
             uri: "https://reactnative.dev/docs/assets/p_cat2.png",
           }}
           style={styles.image}
         />
-
+        {monster && <Monster monster={monster}/>}
         {/* <StatusBar style="auto" /> */}
       </View>
     </ScrollView>
