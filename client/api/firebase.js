5e56122380484930fc39ed6d5fc869e7abee3a51
MarkAlperin <mark.t.alperin@gmail.com>
2022-05-04 12:59:03 -0500

    moved login stuff to .env

--- client/api/firebase.js
+++ client/api/firebase.js
@@ -8,14 +8,8 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDute0OovhzBeveAw4Umfn_UVKd5O8E1mI",
-  authDomain: "clever-goblin.firebaseapp.com",
-  projectId: "clever-goblin",
-  storageBucket: "clever-goblin.appspot.com",
-  messagingSenderId: "705607109690",
-  appId: "1:705607109690:web:b50d7bfada6869aee87903",
-  measurementId: "G-LY519JWY78"
-};
+  //process.env
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
